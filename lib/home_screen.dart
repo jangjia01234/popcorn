13ee94e0dc1738b5a9c4ef68f50c51702668d7fe
Jia <71865277+jangjia01234@users.noreply.github.com>
2025-08-08 23:31:50 +0900

    API 통신 구현

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,7 +1,41 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class HomeScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void getData() async {
+    // var url = Uri.https(
+    //   'https://movies-api.nomadcoders.workers.dev',
+    //   '/popular',
+    // );
+    //
+    // var response = await http.get(url);
+
+    // print(response);
+
+    final response = await http
+        .get(Uri.parse('https://movies-api.nomadcoders.workers.dev/popular'));
+
+    if (response.statusCode == 200) {
+      final jsonData = jsonDecode(response.body);
+      print(jsonData);
+    } else {
+      print('요청 실패: ${response.statusCode}');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +49,17 @@ class HomeScreen extends StatelessWidget {
             Text("가장 인기있는 영화"),
             Text("상영중인 영화"),
             Text("곧 개봉할 영화"),
+            ElevatedButton(
+              onPressed: getData,
+              child: Text(
+                "button",
+                style: TextStyle(
+                  fontSize: 100,
+                  color: Colors.white,
+                  backgroundColor: Colors.red,
+                ),
+              ),
+            )
           ],
         ),
       ),
