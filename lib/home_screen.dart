a00a2d970a987e35acb3e988ec1a7ae856723bc5
Jia <71865277+jangjia01234@users.noreply.github.com>
2025-08-09 00:19:59 +0900

    캐러셀 구현

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,7 +1,57 @@
 import 'dart:convert';
 
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
+const imgList = [
+  "https://images.pexels.com/photos/30971782/pexels-photo-30971782.jpeg",
+  "https://images.pexels.com/photos/3174435/pexels-photo-3174435.jpeg",
+  "https://images.pexels.com/photos/33017020/pexels-photo-33017020.jpeg",
+  "https://images.pexels.com/photos/27532726/pexels-photo-27532726.jpeg",
+];
+
+final List<Widget> imageSliders = imgList
+    .map((item) => Container(
+          margin: EdgeInsets.all(5.0),
+          child: ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              child: Stack(
+                children: <Widget>[
+                  Image.network(item, fit: BoxFit.cover, width: 1000.0),
+                  Positioned(
+                    bottom: 0.0,
+                    left: 0.0,
+                    right: 0.0,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Color.fromARGB(200, 0, 0, 0),
+                            Color.fromARGB(0, 0, 0, 0)
+                          ],
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                        ),
+                      ),
+                      padding: EdgeInsets.symmetric(
+                        vertical: 10.0,
+                        horizontal: 20.0,
+                      ),
+                      child: Text(
+                        'No. ${imgList.indexOf(item)} image',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )),
+        ))
+    .toList();
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -17,15 +67,6 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   void getData() async {
-    // var url = Uri.https(
-    //   'https://movies-api.nomadcoders.workers.dev',
-    //   '/popular',
-    // );
-    //
-    // var response = await http.get(url);
-
-    // print(response);
-
     final response = await http
         .get(Uri.parse('https://movies-api.nomadcoders.workers.dev/popular'));
 
@@ -41,26 +82,110 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home"),
+        backgroundColor: Colors.black,
+        title: Row(
+          children: [
+            CircleAvatar(
+              radius: 10,
+              backgroundColor: Colors.white,
+            ),
+            SizedBox(width: 5),
+            Text(
+              "Popcorn",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w800,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+        centerTitle: false,
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            Text("가장 인기있는 영화"),
-            Text("상영중인 영화"),
-            Text("곧 개봉할 영화"),
-            ElevatedButton(
-              onPressed: getData,
-              child: Text(
-                "button",
-                style: TextStyle(
-                  fontSize: 100,
-                  color: Colors.white,
-                  backgroundColor: Colors.red,
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CarouselSlider(
+                  options: CarouselOptions(
+                    autoPlay: true,
+                    aspectRatio: 1.0,
+                    enlargeCenterPage: true,
+                  ),
+                  items: imageSliders,
                 ),
-              ),
-            )
-          ],
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    "Most Popular",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      for (var movie in ["1", "2", "3"])
+                        Row(
+                          children: [
+                            Container(
+                              color: Colors.white,
+                              width: 120,
+                              height: 160,
+                            ),
+                            SizedBox(width: 10),
+                          ],
+                        ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    "Now in Popcorn",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Text(
+                    "Soon in Popcorn",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+
+                // ElevatedButton(
+                //   onPressed: getData,
+                //   child: Text(
+                //     "button",
+                //     style: TextStyle(
+                //       fontSize: 100,
+                //       color: Colors.white,
+                //       backgroundColor: Colors.red,
+                //     ),
+                //   ),
+                // )
+              ],
+            ),
+          ),
         ),
       ),
     );
